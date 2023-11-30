b14fe68a5bc8c8fd993079516096dd777982f85b
VERA-OBIERO <vera.obiero@gmail.com>
2023-11-30 19:42:13 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagram_list):
+        new_anagrams = []
+        for i in anagram_list:
+            if sorted(i.lower()) == sorted(self.word) and i.lower() != self.word:
+                new_anagrams.append(i)
+        return new_anagrams
+        
